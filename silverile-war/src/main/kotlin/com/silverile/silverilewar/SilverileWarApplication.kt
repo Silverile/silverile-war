63606b283a29b1eca48a84a4b8afc53609f2b11c
silverile <alex.morozovsavs@gmail.com>
2023-08-18 13:40:04 +0200

    Initial commit

---
+++ silverile-war/src/main/kotlin/com/silverile/silverilewar/SilverileWarApplication.kt
@@ -0,0 +1,11 @@
+package com.silverile.silverilewar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SilverileWarApplication
+
+fun main(args: Array<String>) {
+	runApplication<SilverileWarApplication>(*args)
+}
